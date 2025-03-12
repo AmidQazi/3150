49bc81c60ba50a161f5b67d7afb144c2c19fd4a1
AmidQazi <144165570+AmidQazi@users.noreply.github.com>
2025-03-11 20:21:01 -0400

    Create main.cpp

---
+++ Lab4/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "prefixsumoperations.h"
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    int n;
+
+    std::cout << "Enter the size of the array: ";
+    std::cin >> n;
+
+    std::vector<int> result = createNonCrossingPrefixSum(n);
+    std::cout << "Non-crossing Prefix Sum Array: ";
+    for (size_t i = 0; i < result.size(); ++i) {
+        std::cout << result[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
